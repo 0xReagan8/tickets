09154f436a8ff6366591496405d4e40e21aabf98
0XReagan <0XReagan@proton.me>
2024-03-06 09:06:23 -0300

    init

--- generate_codes.py
+++ generate_codes.py
@@ -1,12 +1,18 @@
 import qrcode
 import os
 
+EVENT_ID="Some Event Title"
+# format the event id so we can send in the URL
+EVENT_ID= EVENT_ID.replace(' ', '_' )
+
+
 # Ensure the directory for QR codes exists
 output_dir = "qr_codes"
 os.makedirs(output_dir, exist_ok=True)
 
+base_url = "https://sore-cyan-ostrich-fez.cyclic.app/scan"  # Replace with your actual server address
 # base_url = "https://sore-cyan-ostrich-fez.cyclic.app/scan?id="  # Replace with your actual server address
-base_url = "http://192.168.0.15:5900/scan?id="  # Replace with your actual server address
+# base_url = "http://192.168.0.15:5900/scan?id="  # Replace with your actual server address
 
 for i in range(1, 31):
     # Generate QR code
@@ -16,7 +22,11 @@ for i in range(1, 31):
         box_size=10,
         border=4,
     )
-    url = f"{base_url}{i}"
+    
+    url = f"{base_url}?event_id={EVENT_ID}&ticket_id={i}"  # Added event_id to the URL
+
+    print(url)
+
     qr.add_data(url)
     qr.make(fit=True)
 
@@ -26,5 +36,3 @@ for i in range(1, 31):
     img.save(f"{output_dir}/QR_Code_{i}.png")
 
 print("QR codes generated successfully.")
-
-
