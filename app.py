69e664cf9f7e6f020cecb5ac1997b38373233dea
0XReagan <0XReagan@proton.me>
2024-03-06 11:56:45 -0300

    init

--- app.py
+++ app.py
@@ -9,40 +9,7 @@ WEBHOOL_URL = "https://discord.com/api/webhooks/1214662183452016660/1yOSpSVg3oj0
 
 app = FastAPI()
 
-# async def send_discord_message(message):
-#     webhook_url = WEBHOOL_URL
-#     async with aiohttp.ClientSession() as session:
-#         webhook = message
-#         await session.post(webhook_url, json=webhook)
 
-def build_embed(ticket_id, event_id, timestamp, base_url ):
-
-    embed = {
-        "title": "🚀",
-        "description": f"Event ID: {event_id}\nTicket ID: {ticket_id}\nScan Time: {timestamp}\n\n{base_url}/list",
-        "color": 1543684, 
-        "fields": [],
-        "footer": {
-            "text": "** use report URL to get a text listing of all activity"
-        }
-    }
-
-    return(embed)
-
-def send_discord_message(embed):    
-    # Wrap the embed in a payload as Discord expects
-    payload = {
-        "embeds": [embed],
-    }
-
-    # Convert the payload to JSON and make the POST request to the webhook URL
-    response = requests.post(WEBHOOL_URL, json=payload)
-
-    # Check the response
-    if response.status_code == 204:
-        print("Embed sent successfully!")
-    else:
-        print(f"Failed to send embed. Status code: {response.status_code} - Response: {response.text}")
 
 
 @app.get('/scan')
@@ -51,6 +18,36 @@ async def scan(request: Request):
     base_url = request.base_url
     event_id = request.query_params.get('event_id')
     ticket_id = request.query_params.get('ticket_id')
+
+
+    def build_embed(ticket_id, event_id, timestamp, base_url ):
+
+        embed = {
+            "title": "🚀",
+            "description": f"Event ID: {event_id}\nTicket ID: {ticket_id}\nScan Time: {timestamp}\n\n{base_url}/list",
+            "color": 1543684, 
+            "fields": [],
+            "footer": {
+                "text": "** use report URL to get a text listing of all activity"
+            }
+        }
+
+        return(embed)
+
+    def send_discord_message(embed):    
+        # Wrap the embed in a payload as Discord expects
+        payload = {
+            "embeds": [embed],
+        }
+
+        # Convert the payload to JSON and make the POST request to the webhook URL
+        response = requests.post(WEBHOOL_URL, json=payload)
+
+        # Check the response
+        if response.status_code == 204:
+            print("Embed sent successfully!")
+        else:
+            print(f"Failed to send embed. Status code: {response.status_code} - Response: {response.text}")
 
 
     # Get the current timestamp
