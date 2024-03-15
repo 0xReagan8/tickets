99cb759f53b78e432eba8dd27abb1b71e2df4aae
0XReagan <0XReagan@proton.me>
2024-03-15 03:36:35 -0300

    updated template file and add final data for Kai

--- QR/render_qr_codes.py
+++ QR/render_qr_codes.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import json
 import base64
 from colorama import Fore, Style
@@ -7,6 +8,9 @@ from weasyprint import HTML
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
+from PIL import Image
+import uuid
+
 
 
 # Set the paths
@@ -29,7 +33,7 @@ def image_to_base64(image_path):
         with open(image_path, 'r') as image_file:
             return image_file.read()
 
-def render_tickets():
+def render_tickets(event_name, event_location, event_date, event_time):
     print(f""" 
     {Fore.LIGHTGREEN_EX} 🐟🐟 rendering tickets: {Style.RESET_ALL}
     """)
@@ -38,9 +42,16 @@ def render_tickets():
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # Load event data
-    with open(os.path.join(assets_dir, 'event_data.json'), 'r') as file:
-        event_data = json.load(file)
+    # remove old generated codes
+    for filename in os.listdir(output_dir):
+        file_path = os.path.join(output_dir, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
 
     # Setup Jinja2 environment
     env = Environment(loader=FileSystemLoader(templates_dir))
@@ -68,10 +79,12 @@ def render_tickets():
                 bg_image=image_to_base64(os.path.join(assets_dir, 'bg.png')),
                 logo_image=image_to_base64(os.path.join(assets_dir, 'logo_autonomous.svg')),
                 qr_code_image=image_to_base64(os.path.join(qr_codes_dir, qr_code_file)),
-                event_logo=image_to_base64(os.path.join(assets_dir, 'event_logo.png')),
-                event_title=event_data['event_name'],
-                event_date=event_data['event_date'],
-                event_time=event_data['event_time']
+                event_logo=image_to_base64(os.path.join(assets_dir, 'event_logo.png')),                
+                event_title=event_name,
+                event_location=event_location,
+                event_date=event_date,
+                event_time=event_time,
+                lion_lock_code=uuid.uuid4()
             )
             # Define output filenames
             base_filename = qr_code_file.split('.')[0]
@@ -83,15 +96,20 @@ def render_tickets():
             with open(html_filename, 'w') as file:
                 file.write(rendered_html)
 
-            # Convert HTML to PDF using WeasyPrint
-            HTML(string=rendered_html, base_url=output_dir).write_pdf(pdf_filename)
-
             # Convert HTML to PNG using Selenium
             driver.get(f'file://{html_filename}')
-            driver.set_window_size(1200, 800)  # Adjust as needed
+            driver.set_window_size(820, 1180)  # Adjust as needed
             driver.save_screenshot(png_filename)
 
+            # Convert PNG to PDF
+            image = Image.open(png_filename)
+            image.convert('RGB').save(pdf_filename, 'PDF')
+
     driver.quit()
+
     print(f""" 
+
     {Fore.WHITE} 🦞 Processing complete 🦞... {Style.RESET_ALL} 
+
     """)
+    
